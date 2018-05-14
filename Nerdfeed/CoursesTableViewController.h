8fc7acaaf887ef30b304cd6fe7e5b74010928b39
Zaur Giyasov <zagiyas@mera.ru>
2018-05-14 13:49:49 +0300

    first - prepare

---
+++ Nerdfeed/CoursesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoursesTableViewController.h
+//  Nerdfeed
+//
+//  Created by Zaur Giyasov on 14/05/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoursesTableViewController : UITableViewController
+
+@end
---
+++ Nerdfeed/CoursesTableViewController.m
@@ -0,0 +1,28 @@
+//
+//  CoursesTableViewController.m
+//  Nerdfeed
+//
+//  Created by Zaur Giyasov on 14/05/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import "CoursesTableViewController.h"
+
+@interface CoursesTableViewController ()
+
+@end
+
+@implementation CoursesTableViewController
+
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+
+@end
